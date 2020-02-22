58aec82eebddad827868fe5e67442305f552f3f3
Mitzi Estrada <m_estrada17@live.com>
2020-02-22 06:45:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,8 @@ def key_for_min_value(name_hash)
   lowest_key = nil
   lowest_value = nil
   name_hash.each |key, value|
-    if lowest_value == nil || lowest_key < key
+    if lowest_value == nil || value < lowest_value
+      
       
   end
 
